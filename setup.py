11d6966dec76cd1965cdbfbce0eaebaa7f8efc62
mztkfka <tkfka1@korea.com>
2024-01-23 10:46:13 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hankyo", # Replace with your own username
+    version="0.0.1",
+    author="Hankyo Jeong",
+    author_email="tkfka1@korea.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tkfka1/hankyo_pypi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
+# install_requires=['tqdm', 'pandas', 'scikit-learn',],
+# keywords=['teddynote', 'teddylee777', 'python datasets', 'python tutorial', 'pypi'],
+# package_data={},
+# zip_safe=False,
+# classifiers=[
+#     'Programming Language :: Python :: 3.6',
+#     'Programming Language :: Python :: 3.7',
+#     'Programming Language :: Python :: 3.8',
+#     'Programming Language :: Python :: 3.9',
+# ],
